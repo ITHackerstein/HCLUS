fe6d59b14b6e76ce98d8271814671102a806a79e
ITHackerstein <carelladavide1@gmail.com>
2024-07-03 15:52:40 +0200

    exceptions: Added missing JavaDoc comments to exceptions.

--- src/main/java/com/davidecarella/exceptions/InvalidDepthException.java
+++ src/main/java/com/davidecarella/exceptions/InvalidDepthException.java
@@ -1,6 +1,16 @@
 package com.davidecarella.exceptions;
 
+import com.davidecarella.clustering.HierarchicalClusterMiner;
+
+/**
+ * Eccezione lanciata quando si utilizza una profondità errata per un {@link HierarchicalClusterMiner miner}.
+ */
 public class InvalidDepthException extends Exception {
+    /**
+     * Costruisce l'eccezione con messaggio {@code message}, specificato come parametro.
+     *
+     * @param message il messaggio dell'eccezione
+     */
     public InvalidDepthException(String message) {
         super(message);
     }
--- src/main/java/com/davidecarella/exceptions/InvalidSizeException.java
+++ src/main/java/com/davidecarella/exceptions/InvalidSizeException.java
@@ -1,6 +1,16 @@
 package com.davidecarella.exceptions;
 
+import com.davidecarella.data.Example;
+
+/**
+ * Eccezione lanciata quando si prova a calcolare la distanza fra due {@link Example esempi} con lunghezze diverse.
+ */
 public class InvalidSizeException extends Exception {
+    /**
+     * Costruisce l'eccezione con messaggio {@code message}, specificato come parametro.
+     *
+     * @param message il messaggio dell'eccezione
+     */
     public InvalidSizeException(String message) {
         super(message);
     }
