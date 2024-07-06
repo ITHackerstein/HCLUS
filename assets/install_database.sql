b4c6151d61a39381ef53254b9cd420be7a4f0514
ITHackerstein <carelladavide1@gmail.com>
2024-07-06 10:35:50 +0200

    assets: Added another example table to use for testing.

--- assets/install_database.sql
+++ assets/install_database.sql
@@ -4,10 +4,12 @@ CREATE DATABASE IF NOT EXISTS hclus_db;
 USE hclus_db;
 
 CREATE TABLE IF NOT EXISTS examples_1(X1 FLOAT, X2 FLOAT, X3 FLOAT);
+CREATE TABLE IF NOT EXISTS examples_2(X1 FLOAT, X2 FLOAT, X3 FLOAT);
 CREATE TABLE IF NOT EXISTS empty_examples(X1 FLOAT, X2 FLOAT, X3 FLOAT);
 CREATE TABLE IF NOT EXISTS no_numbers(description VARCHAR(32), value FLOAT);
 
 INSERT INTO examples_1 VALUES (1, 2, 0), (0, 1, -1), (1, 3, 5), (1, 3, 4), (2, 2, 0);
+INSERT INTO examples_2 VALUES (1, 2, 0), (0, 1, -1), (1, 3, 5);
 INSERT INTO no_numbers VALUES ("a", 1), ("b", 2), ("c", 3);
 
 GRANT ALL PRIVILEGES ON hclus_db.* TO hclus_user;
