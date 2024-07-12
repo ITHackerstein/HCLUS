a0a330c3221383c444e24f4bf13159361899a55c
ITHackerstein <carelladavide1@gmail.com>
2024-07-12 11:46:17 +0200

    DendrogramViewerWidget: Moved the drawing code to the `DendrogramViewerWidget`. Now we use `AffineTransforms` to move and zoom around the canvas.

--- client/src/main/java/com/davidecarella/hclus/client/gui/DendrogramViewerWidget.java
+++ client/src/main/java/com/davidecarella/hclus/client/gui/DendrogramViewerWidget.java
@@ -4,133 +4,133 @@ import com.davidecarella.hclus.common.Clustering;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
 import java.util.Stack;
 
-public class DendrogramViewerWidget extends JPanel implements MouseMotionListener {
-    private class Dendrogram {
-        private int clusterSize = 32;
-        private int clusterSpacing = 16;
-        private int clusterBorderSize = 6;
-        private int edgeLineWidth = 1;
-        private Color clusterColor = new Color(138, 43, 216);
-        private Color edgeColor = Color.black;
+class Dendrogram {
+    static final int CLUSTER_SIZE = 32;
+    static final int CLUSTER_SPACING = 16;
+    static final int CLUSTER_BORDER_SIZE = 6;
+    static final int EDGE_LINE_WIDTH = 1;
+    static final Color CLUSTER_COLOR = new Color(138, 43, 216);
+    static final Color EDGE_COLOR = Color.black;
 
-        private int[] leafClusterIndexMap;
-        private Point[] clusterPositions;
-        private int[][] edges;
+    private Clustering clustering = null;
+    private final int[] leafClusterIndexMap;
+    private final Point[] clusterPositions;
+    private final int[][] edges;
 
-        public Dendrogram() {
-            this.leafClusterIndexMap = new int[clustering.exampleCount()];
-            this.clusterPositions = new Point[clustering.exampleCount() + clustering.steps().length];
-            this.edges = new int[this.clusterPositions.length * 2][2];
-            this.initializeArrays();
+    Dendrogram(Clustering clustering) {
+        this.clustering = clustering;
+        this.leafClusterIndexMap = new int[this.clustering.exampleCount()];
+        this.clusterPositions = new Point[this.clustering.exampleCount() + this.clustering.steps().length];
+        this.edges = new int[this.clusterPositions.length * 2][2];
+        this.initializeArrays();
+    }
+
+    private void initializeArrays() {
+        var stack = new Stack<Integer>();
+        stack.push(this.clusterPositions.length - 1);
+
+        int leafIndex = 0;
+        int edgeIndex = 0;
+        while (!stack.empty()) {
+            var clusterIndex = stack.pop();
+            if (clusterIndex < this.clustering.exampleCount()) {
+                this.leafClusterIndexMap[clusterIndex] = leafIndex;
+                this.clusterPositions[leafIndex] = new Point((CLUSTER_SPACING + CLUSTER_SIZE * 2) * (leafIndex - this.clustering.exampleCount() / 2), 0);
+                ++leafIndex;
+                continue;
+            }
+
+            var createStep = this.clustering.steps()[clusterIndex - this.clustering.exampleCount()];
+
+            stack.push(createStep.firstClusterIndex());
+            stack.push(createStep.secondClusterIndex());
+
+            this.edges[edgeIndex][0] = clusterIndex;
+            this.edges[edgeIndex][1] = createStep.firstClusterIndex();
+            ++edgeIndex;
+
+            this.edges[edgeIndex][0] = clusterIndex;
+            this.edges[edgeIndex][1] = createStep.secondClusterIndex();
+            ++edgeIndex;
         }
 
-        private int getActualClusterIndex(int clusterIndex) {
-            var result = clusterIndex;
-            if (result < clustering.exampleCount()) {
-                result = this.leafClusterIndexMap[result];
-            }
-            return result;
-        }
-
-        private void fillCenteredCircle(Graphics2D g2d, int x, int y, int radius) {
-            g2d.fillArc(x - radius, y - radius, radius * 2, radius * 2, 0, 360);
-        }
-
-        private void initializeArrays() {
-            var stack = new Stack<Integer>();
-            stack.push(this.clusterPositions.length - 1);
-
-            int leafIndex = 0;
-            int edgeIndex = 0;
-            while (!stack.empty()) {
-                var clusterIndex = stack.pop();
-                if (clusterIndex < clustering.exampleCount()) {
-                    this.leafClusterIndexMap[clusterIndex] = leafIndex;
-                    clusterPositions[leafIndex] = new Point(
-                        canvasRectangle.width / 2 + (this.clusterSpacing + this.clusterSize * 2) * (leafIndex - clustering.exampleCount() / 2),
-                        canvasRectangle.height / 2
-                    );
-
-                    ++leafIndex;
-                    continue;
-                }
-
-                var createStep = clustering.steps()[clusterIndex - clustering.exampleCount()];
-
-                stack.push(createStep.firstClusterIndex());
-                stack.push(createStep.secondClusterIndex());
-
-                edges[edgeIndex][0] = clusterIndex;
-                edges[edgeIndex][1] = createStep.firstClusterIndex();
-                ++edgeIndex;
-                edges[edgeIndex][0] = clusterIndex;
-                edges[edgeIndex][1] = createStep.secondClusterIndex();
-                ++edgeIndex;
-            }
-
-            for (int i = 0; i < clustering.steps().length; ++i) {
-                var step = clustering.steps()[i];
-                var firstClusterPosition = clusterPositions[getActualClusterIndex(step.firstClusterIndex())];
-                var secondClusterPosition = clusterPositions[getActualClusterIndex(step.secondClusterIndex())];
-                clusterPositions[clustering.exampleCount() + i] = new Point(
-                    (int) ((firstClusterPosition.getX() + secondClusterPosition.getX()) / 2),
-                    (int) (Math.min(firstClusterPosition.getY(), secondClusterPosition.getY()) - this.clusterSpacing - this.clusterSize * 2)
-                );
-            }
-        }
-
-        public void paint(Graphics2D g2d) {
-            for (var edge : this.edges) {
-                var firstIndex = getActualClusterIndex(edge[0]);
-                var secondIndex = getActualClusterIndex(edge[1]);
-
-                g2d.setColor(this.edgeColor);
-                g2d.setStroke(new BasicStroke(this.edgeLineWidth));
-                g2d.drawLine(
-                    clusterPositions[firstIndex].x, clusterPositions[firstIndex].y,
-                    clusterPositions[secondIndex].x, clusterPositions[secondIndex].y
-                );
-            }
-
-            for (var clusterPosition : clusterPositions) {
-                var distance = mousePosition.distance(clusterPosition);
-                System.out.println(distance);
-                var color1 = this.clusterColor.darker();
-                var color2 = this.clusterColor;
-                if (distance <= this.clusterSize) {
-                    color1 = color1.brighter();
-                    color2 = color2.brighter();
-                }
-
-                g2d.setColor(color1);
-                fillCenteredCircle(g2d, clusterPosition.x, clusterPosition.y, this.clusterSize);
-                g2d.setColor(color2);
-                fillCenteredCircle(g2d, clusterPosition.x, clusterPosition.y, this.clusterSize - this.clusterBorderSize);
-            }
-
-            System.out.println();
+        for (int i = 0; i < this.clustering.steps().length; ++i) {
+            var step = this.clustering.steps()[i];
+            var firstClusterPosition = this.clusterPositions[getActualClusterIndex(step.firstClusterIndex())];
+            var secondClusterPosition = this.clusterPositions[getActualClusterIndex(step.secondClusterIndex())];
+            this.clusterPositions[this.clustering.exampleCount() + i] = new Point(
+                (int) ((firstClusterPosition.getX() + secondClusterPosition.getX()) / 2),
+                (int) (Math.min(firstClusterPosition.getY(), secondClusterPosition.getY()) - CLUSTER_SPACING - CLUSTER_SIZE * 2)
+            );
         }
     }
 
-    private Clustering clustering = null;
-    private Dendrogram dendrogram = null;
-    private Rectangle canvasRectangle = new Rectangle();
+    int getActualClusterIndex(int clusterIndex) {
+        var result = clusterIndex;
+        if (result < this.clustering.exampleCount()) {
+            result = this.leafClusterIndexMap[result];
+        }
+        return result;
+    }
 
-    private Point mousePosition = new Point();
+    int getClusterCount() {
+        return this.clusterPositions.length;
+    }
+
+    Point getClusterPosition(int clusterIndex) {
+        return this.clusterPositions[clusterIndex];
+    }
+
+    int getEdgeCount() {
+        return this.edges.length;
+    }
+
+    int[] getEdge(int edgeIndex) {
+        return this.edges[edgeIndex];
+    }
+}
+
+public class DendrogramViewerWidget extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener {
+    private Dendrogram dendrogram = null;
+    private int selectedClusterIndex = -1;
+    private int hoveredClusterIndex = -1;
+
+    private AffineTransform transform = null;
+
+    private Point lastMousePosition = new Point();
+    private int pressedMouseButtons = 0;
 
     public DendrogramViewerWidget() {
         this.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Dendrogramma"));
+        this.addMouseListener(this);
         this.addMouseMotionListener(this);
+        this.addMouseWheelListener(this);
     }
 
     public void setClustering(Clustering clustering) {
-        this.clustering = clustering;
-        this.dendrogram = new Dendrogram();
+        this.dendrogram = new Dendrogram(clustering);
         repaint();
+    }
+
+    private void fillCenteredCircle(Graphics2D g2d, int x, int y, int radius) {
+        g2d.fillArc(x - radius, y - radius, radius * 2, radius * 2, 0, 360);
+    }
+
+    private Point inverseTransformPoint(Point point) {
+        Point result = new Point();
+        try {
+            this.transform.inverseTransform(point, result);
+        } catch (NoninvertibleTransformException ignored) {
+            // NOTE: This can never happen because we construct the matrix only using `translate`, `scale` and the
+            //       multiplication operator, so it is always invertible
+        }
+
+        return result;
     }
 
     @Override
@@ -139,18 +139,17 @@ public class DendrogramViewerWidget extends JPanel implements MouseMotionListene
     }
 
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         var g2d = (Graphics2D) g;
 
         var insets = this.getBorder().getBorderInsets(this);
-        this.canvasRectangle = new Rectangle(insets.left, insets.top, getWidth() - insets.left - insets.right, getHeight() - insets.top - insets.bottom);
+        var canvasRectangle = new Rectangle(insets.left, insets.top, getWidth() - insets.left - insets.right, getHeight() - insets.top - insets.bottom);
 
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
-        if (this.clustering == null || !this.isEnabled()) {
+        if (this.dendrogram == null || !this.isEnabled()) {
             g2d.setFont(g2d.getFont().deriveFont(18.0f));
 
             var text = "Nessun dendrogramma caricato!";
@@ -165,19 +164,152 @@ public class DendrogramViewerWidget extends JPanel implements MouseMotionListene
             return;
         }
 
-        g2d.clearRect(this.canvasRectangle.x, this.canvasRectangle.y, this.canvasRectangle.width, this.canvasRectangle.height);
-        g2d.setClip(this.canvasRectangle.x, this.canvasRectangle.y, this.canvasRectangle.width, this.canvasRectangle.height);
+        if (this.transform == null) {
+            this.transform = AffineTransform.getTranslateInstance(canvasRectangle.width / 2.0, canvasRectangle.height / 2.0);
+        }
 
-        this.dendrogram.paint(g2d);
+        var oldClip = g2d.getClip();
+        g2d.setClip(canvasRectangle.x, canvasRectangle.y, canvasRectangle.width, canvasRectangle.height);
+        g2d.clearRect(canvasRectangle.x, canvasRectangle.y, canvasRectangle.width, canvasRectangle.height);
+        g2d.setTransform(this.transform);
+
+        for (int i = 0; i < this.dendrogram.getEdgeCount(); ++i) {
+            var edge = this.dendrogram.getEdge(i);
+
+            var firstIndex = this.dendrogram.getActualClusterIndex(edge[0]);
+            var firstPosition = this.dendrogram.getClusterPosition(firstIndex);
+
+            var secondIndex = this.dendrogram.getActualClusterIndex(edge[1]);
+            var secondPosition = this.dendrogram.getClusterPosition(secondIndex);
+
+            g2d.setColor(Dendrogram.EDGE_COLOR);
+            g2d.setStroke(new BasicStroke(Dendrogram.EDGE_LINE_WIDTH));
+            g2d.drawLine(firstPosition.x, firstPosition.y, secondPosition.x, secondPosition.y);
+        }
+
+        for (int i = 0; i < this.dendrogram.getClusterCount(); ++i) {
+            var index = this.dendrogram.getActualClusterIndex(i);
+            var clusterPosition = this.dendrogram.getClusterPosition(index);
+
+            var color1 = Dendrogram.CLUSTER_COLOR.darker();
+            var color2 = Dendrogram.CLUSTER_COLOR;
+            if (this.selectedClusterIndex == index || this.hoveredClusterIndex == index)
+            {
+                color1 = color1.brighter();
+                color2 = color2.brighter();
+            }
+
+            g2d.setColor(color1);
+            fillCenteredCircle(g2d, clusterPosition.x, clusterPosition.y, Dendrogram.CLUSTER_SIZE);
+            g2d.setColor(color2);
+            fillCenteredCircle(g2d, clusterPosition.x, clusterPosition.y, Dendrogram.CLUSTER_SIZE - Dendrogram.CLUSTER_BORDER_SIZE);
+        }
+
+        // paintClusterInfoTooltip();
+        g2d.setTransform(new AffineTransform());
+        g2d.setClip(oldClip);
     }
 
     @Override
     public void mouseDragged(MouseEvent event) {
+        if (this.dendrogram == null || !this.isEnabled() || this.pressedMouseButtons != MouseEvent.BUTTON2) {
+            return;
+        }
+
+        if (this.lastMousePosition != null) {
+            this.transform.translate(event.getX() - this.lastMousePosition.getX(), event.getY() - this.lastMousePosition.getY());
+        }
+
+        this.lastMousePosition = event.getPoint();
+        this.repaint();
     }
 
     @Override
     public void mouseMoved(MouseEvent event) {
-        this.mousePosition = event.getPoint();
-        repaint();
+        this.lastMousePosition = event.getPoint();
+
+        if (this.dendrogram == null || !this.isEnabled() || this.pressedMouseButtons != MouseEvent.NOBUTTON) {
+            return;
+        }
+
+        var mousePosition = this.inverseTransformPoint(event.getPoint());
+        for (int i = 0; i < this.dendrogram.getClusterCount(); ++i) {
+            var index = this.dendrogram.getActualClusterIndex(i);
+            var clusterPosition = this.dendrogram.getClusterPosition(index);
+            if (mousePosition.distance(clusterPosition) <= Dendrogram.CLUSTER_SIZE) {
+                this.hoveredClusterIndex = index;
+                this.repaint();
+                return;
+            }
+        }
+
+        this.hoveredClusterIndex = -1;
+        this.repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+        if (this.dendrogram == null || !this.isEnabled() || event.getButton() != MouseEvent.BUTTON1) {
+            return;
+        }
+
+        var mousePosition = this.inverseTransformPoint(event.getPoint());
+        for (int i = 0; i < this.dendrogram.getClusterCount(); ++i) {
+            var index = this.dendrogram.getActualClusterIndex(i);
+            var clusterPosition = this.dendrogram.getClusterPosition(index);
+            if (mousePosition.distance(clusterPosition) <= Dendrogram.CLUSTER_SIZE) {
+                this.selectedClusterIndex = index;
+                this.repaint();
+                return;
+            }
+        }
+
+        this.selectedClusterIndex = -1;
+        this.repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event) {
+        if (this.dendrogram == null || !this.isEnabled()) {
+            return;
+        }
+
+        this.pressedMouseButtons |= event.getButton();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+        if (this.dendrogram == null || !this.isEnabled()) {
+            return;
+        }
+
+        this.pressedMouseButtons &= ~event.getButton();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent event) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent event) {
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent event) {
+        if (this.dendrogram == null || !this.isEnabled()) {
+            return;
+        }
+
+        int notches = event.getWheelRotation();
+        var zoom = Math.exp(-Math.signum(notches) * 0.02);
+
+        var newTransform = new AffineTransform();
+        newTransform.translate(event.getX(), event.getY());
+        newTransform.scale(zoom, zoom);
+        newTransform.translate(-event.getX(), -event.getY());
+        newTransform.concatenate(this.transform);
+        this.transform = newTransform;
+
+        this.repaint();
     }
 }
